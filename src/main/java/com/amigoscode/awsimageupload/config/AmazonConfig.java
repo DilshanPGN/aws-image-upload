c77b59fe38a47cd79b12f49e7239459bc6fbec28
Nipun Dilshan <73216265+DilshanPGN@users.noreply.github.com>
2023-06-27 08:46:55 +0530

    Update AmazonConfig.java
    reomoving credentials

--- src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
@@ -8,16 +8,22 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class AmazonConfig {
+
+    @value("${aws.accesskey}")
+    private String accessKey;
+    @value("${aws.secretkey}")
+    private String secretKey;
 
     @Bean
     public AmazonS3 s3(){
 
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIA35KSOA5OFOQ65DZC",
-                "wZr9JA4ZoX/aQ11+WVVaMGOSG+ReVkiFYU8Z56zU"
+                accessKey,
+                secretKey
         );
 
         return AmazonS3ClientBuilder
